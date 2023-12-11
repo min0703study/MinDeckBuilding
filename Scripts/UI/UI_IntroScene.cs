77a081ef5a80657e9b572a584f27e249a64a3769
min0703study <min0703study@gmail.com>
2023-12-11 19:44:30 +0900

    TextMeshPro / Addressable 추가

--- Scripts/UI/UI_IntroScene.cs
+++ Scripts/UI/UI_IntroScene.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI_IntroScene : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{	
+	[SerializeField]
+	private Slider loadingSlider;
+	
+	[SerializeField]
+	private Button startGameButton;
+	
+	protected void Awake()
+	{
+		startGameButton.onClick.AddListener(() => SceneManager.LoadScene("GameScene"));
+		ResourceManager.Instance.LoadAllAsync("PreLoad", (key, count, totalCount) =>
+		{
+			loadingSlider.value = (float)count/totalCount;
+			if (count == totalCount)
+			{
+				loadingSlider.gameObject.SetActive(false);
+				startGameButton.gameObject.SetActive(true);
+			}
+		});
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
 }
